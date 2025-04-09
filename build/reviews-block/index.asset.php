35cd96845fdab80d0f728b1dba6e784f157c479f
Jasper Frumau <jasper@imagewize.com>
2025-04-09 12:19:50 +0700

    Style & Security Update

--- build/reviews-block/index.asset.php
+++ build/reviews-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'a5c82a702bd3ef7ecd05');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '5ea01c58dd83c965ff7f');
